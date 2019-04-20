62e36e0d4bb46b9a11f8b57a670bd676084e3e29
Fodor Dániel <daniel.fodor@benestudio.co>
2019-04-20 17:40:19 +0200

    add Network module

---
+++ app/src/main/java/com/example/todoapp/network/NetworkConfig.kt
@@ -0,0 +1,5 @@
+package com.example.todoapp.network
+
+object NetworkConfig {
+    const val API_ENDPOINT = "https://jsonplaceholder.typicode.com/"
+}
--- app/src/main/java/com/example/todoapp/network/TodoApi.kt
+++ app/src/main/java/com/example/todoapp/network/TodoApi.kt
@@ -1,4 +1,39 @@
 package com.example.todoapp.network
 
+import com.example.todoapp.model.Todo
+import com.example.todoapp.model.TodoResult
+import com.example.todoapp.model.TodoUpdate
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.PUT
+import retrofit2.http.DELETE
+
+
+
+
+
 interface TodoApi {
+    @GET("todos")
+    fun getTodos(): Call<TodoResult>
+
+    @POST("todos")
+    fun createTodo(@Body body: TodoUpdate): Call<Void>
+
+    @GET("todos/{todoId}")
+    fun getTodoById(
+        @Path("todoId") todoId: Int?
+    ): Call<Todo>
+
+    @PUT("todos/{todoId}")
+    fun updateTodo(
+        @Path("todoId") todoId: Int?, @Body body: TodoUpdate
+    ): Call<Void>
+
+    @DELETE("todos/{todoId}")
+    fun deleteTodo(
+        @Path("todoId") todoId: Int?
+    ): Call<Void>
 }
